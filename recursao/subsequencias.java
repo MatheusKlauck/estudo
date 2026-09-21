7f412925e7e42ac8329ac9dd2e5a0d0a18741f12
Matheus_k <matheusklauck8@gmail.com>
2026-09-21 20:04:02 -0300

    Add recursive algorithm for array subsequences

---
+++ recursao/subsequencias.java
@@ -0,0 +1,52 @@
+package estudo.recursao;
+
+public class subsequencias {
+    static String displayArr(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1)
+                sb.append(",");
+        }
+
+        sb.append("]");
+
+        // System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    static int[] NovoRamo(int[] arrBase, int[] arrAtual, int indiceAtual) {
+        int[] nwSubs = new int[arrAtual.length + 1];
+        // copia os valores atuais da subsequência
+        for (int i = 0; i < arrAtual.length; i++) {
+            nwSubs[i] = arrAtual[i];
+        }
+        nwSubs[nwSubs.length - 1] = arrBase[indiceAtual];
+        System.out.println("            n.r add(" + arrBase[indiceAtual] + ")");
+        return nwSubs;
+    }
+
+    static void GerarSequencia(int[] arrBase, int indiceAtual, int[] ramoAtual) {
+        if (indiceAtual >= arrBase.length) {
+            // Ao percorrer todo o array base, exibe a subsequencia gerada
+            System.out.println("=>subs:" + displayArr(ramoAtual));
+            return;
+        } else {
+            System.out.println("        ramo atual:" + displayArr(ramoAtual));
+        }
+
+        // gera "ramo" que usará o valor do array base na posição "indiceAtual"
+        int[] novoRamo = NovoRamo(arrBase, ramoAtual, indiceAtual);
+        System.out.println("            nova subs c/ nr");
+        GerarSequencia(arrBase, indiceAtual + 1, novoRamo); // Explora o novo ramo, que inclui o elemento mais recente
+        System.out.println("            nova subs s/ nr");
+        GerarSequencia(arrBase, indiceAtual + 1, ramoAtual);// Explora o ramo sem o elemento mais recente
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        GerarSequencia(arr, 0, new int[0]);
+    }
+}
